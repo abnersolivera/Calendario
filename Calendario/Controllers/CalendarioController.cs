e78f6c11ed9a414dfc0be73dcf126b7f66f6c9a2
Abner Santos <abner.santos@bdsdatasolution.com.br>
2023-05-08 19:49:28 -0300

    novo endpoint

--- Calendario/Controllers/CalendarioController.cs
+++ Calendario/Controllers/CalendarioController.cs
@@ -1,5 +1,7 @@
 ﻿using Calendario.Servico;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace Calendario.Controllers
 {
@@ -16,10 +18,17 @@ namespace Calendario.Controllers
             _calendarService = calendarService;
         }
 
+        [HttpGet]
+        public async Task<IActionResult> GetAgendas()
+        {
+            var agendas = _calendarService.ListCalendars();
+            return Ok(agendas);
+        }
+
         [HttpGet("{calendarId}")]
         public async Task<IActionResult> GetCalendar(string calendarId)
         {
-            var calendar = await _calendarService._calendarService.Calendars.Get(calendarId).ExecuteAsync();
+            var calendar = await _calendarService.GetCalendar(calendarId);
             return Ok(calendar);
         }
 
@@ -27,6 +36,15 @@ namespace Calendario.Controllers
         public async Task<IActionResult> GetCalendarEvents(string calendarId, int maxResults)
         {
             var events = _calendarService.GetEvents(calendarId, maxResults);
+            return Ok(events);
+        }
+
+        [HttpGet("Eventos")]
+        [SwaggerUI(SwaggerUIType.DatePicker, "dataInicio", "Data de início", "Selecione a data de início", "dataFinal", "Data final", "Selecione a data final")]
+        public async Task<IActionResult> GetTodosEventos(DateTime dataInicio, DateTime dataFinal)
+        {
+            var events = _calendarService.GetEvents(dataInicio, dataFinal);
+
             return Ok(events);
         }
 
--- Calendario/Servico/GoogleCalendarService.cs
+++ Calendario/Servico/GoogleCalendarService.cs
@@ -27,5 +27,38 @@ public class GoogleCalendarService
         IList<Event> items = events.Items;
         return items;
     }
+
+    public IList<Event> GetEvents(DateTime dataInicio, DateTime dataFinal)
+    {
+        var calendarios = ListCalendars();
+
+        var events = new List<Event>();
+
+        foreach (var item in calendarios)
+        {
+            var id = item.Id;
+            EventsResource.ListRequest request = _calendarService.Events.List(id);
+            request.TimeMin = dataInicio;
+            request.TimeMax = dataFinal;
+            var result = request.Execute();
+            events.AddRange(result.Items);
+        }
+        
+        return events;
+    }
+
+    public IList<CalendarListEntry> ListCalendars()
+    {
+        var request = _calendarService.CalendarList.List();
+        var calendarList = request.Execute();
+        return calendarList.Items;
+
+    }
+
+    public async Task<Calendar> GetCalendar(string calendarId)
+    {
+        return await _calendarService.Calendars.Get(calendarId).ExecuteAsync();
+    }
+
 }
 
